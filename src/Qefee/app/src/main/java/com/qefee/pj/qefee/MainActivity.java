29c5f0171b9dabe0a259ab55d6c77dfcf438bde6
tongjin <aotian16@gmail.com>
2016-09-29 16:16:46 +0800

    add main menu

--- src/Qefee/app/src/main/java/com/qefee/pj/qefee/MainActivity.java
+++ src/Qefee/app/src/main/java/com/qefee/pj/qefee/MainActivity.java
@@ -2,6 +2,9 @@ package com.qefee.pj.qefee;
 
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.qefee.pj.qefee.activity.BaseActivity;
 
@@ -16,5 +19,25 @@ public class MainActivity extends BaseActivity {
         setSupportActionBar(myToolbar);
 
         System.out.println("tongjin");
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_favorite:
+                i("click action_favorite");
+                return true;
+            case R.id.action_settings:
+                i("click action_settings");
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }

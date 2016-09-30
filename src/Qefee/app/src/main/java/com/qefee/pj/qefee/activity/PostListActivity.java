b3e056bb9ddf63f33c3fca3aa062c285b7b8a812
tongjin <aotian16@gmail.com>
2016-09-30 14:01:16 +0800

    connect activity

--- src/Qefee/app/src/main/java/com/qefee/pj/qefee/activity/PostListActivity.java
+++ src/Qefee/app/src/main/java/com/qefee/pj/qefee/activity/PostListActivity.java
@@ -1,10 +1,13 @@
 package com.qefee.pj.qefee.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.qefee.pj.qefee.R;
@@ -28,4 +31,42 @@ public class PostListActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.menu_post_list, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                // User chose the "Settings" item, show the app settings UI...
+                startSettingsActivity();
+                return true;
+
+            case R.id.action_login:
+                // User chose the "Favorite" action, mark the current item
+                // as a favorite...
+                startLoginActivity();
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+
+    private void startLoginActivity() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+    private void startSettingsActivity() {
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);
+    }
 }

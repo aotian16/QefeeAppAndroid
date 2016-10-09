da84cdf48f3a4586034c315e1fc1def0ea1ae56e
tongjin <aotian16@gmail.com>
2016-10-09 16:39:52 +0800

    hideKeyboard

--- src/Qefee/app/src/main/java/com/qefee/pj/qefee/activity/base/BaseActivity.java
+++ src/Qefee/app/src/main/java/com/qefee/pj/qefee/activity/base/BaseActivity.java
@@ -1,6 +1,11 @@
 package com.qefee.pj.qefee.activity.base;
 
+import android.content.Context;
 import android.content.Intent;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 
 import com.qefee.pj.ebase.activity.EBaseActivity;
 
@@ -17,5 +22,51 @@ public abstract class BaseActivity extends EBaseActivity {
     protected void startActivity(Class<?> cls) {
         Intent intent = new Intent(this, cls);
         startActivity(intent);
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        if (ev.getAction() == MotionEvent.ACTION_DOWN) {
+            View v = getCurrentFocus();
+            if (isShouldHideKeyboard(v, ev)) {
+                hideKeyboard(v);
+            }
+        }
+        return super.dispatchTouchEvent(ev);
+    }
+
+    /**
+     * 根据EditText所在坐标和用户点击的坐标相对比，来判断是否隐藏键盘，因为当用户点击EditText时则不能隐藏
+     *
+     * @param v     view
+     * @param event event
+     * @return result
+     */
+    private boolean isShouldHideKeyboard(View v, MotionEvent event) {
+        if (v != null && (v instanceof EditText)) {
+            int[] l = {0, 0};
+            v.getLocationInWindow(l);
+            int left = l[0],
+                    top = l[1],
+                    bottom = top + v.getHeight(),
+                    right = left + v.getWidth();
+            return !(event.getX() > left && event.getX() < right
+                    && event.getY() > top && event.getY() < bottom);
+        }
+        // 如果焦点不是EditText则忽略，这个发生在视图刚绘制完，第一个焦点不在EditText上，和用户用轨迹球选择其他的焦点
+        return false;
+    }
+
+    /**
+     * 获取InputMethodManager，隐藏软键盘
+     *
+     * @param v view
+     */
+    private void hideKeyboard(View v) {
+        if (v.getWindowToken() != null) {
+            InputMethodManager im = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            im.hideSoftInputFromWindow(v.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+        }
+        v.clearFocus();
     }
 }

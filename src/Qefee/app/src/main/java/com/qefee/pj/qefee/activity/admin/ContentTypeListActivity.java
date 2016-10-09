f0325d9a3b4f05b855d8892772aaef5b5b2e59c2
tongjin <aotian16@gmail.com>
2016-10-09 11:39:14 +0800

    setDisplayHomeAsUpEnabled

--- src/Qefee/app/src/main/java/com/qefee/pj/qefee/activity/admin/ContentTypeListActivity.java
+++ src/Qefee/app/src/main/java/com/qefee/pj/qefee/activity/admin/ContentTypeListActivity.java
@@ -3,6 +3,7 @@ package com.qefee.pj.qefee.activity.admin;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
@@ -17,6 +18,11 @@ public class ContentTypeListActivity extends BaseActivity {
         setContentView(R.layout.activity_content_type_list);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        ActionBar ab = getSupportActionBar();
+
+        // Enable the Up button
+        ab.setDisplayHomeAsUpEnabled(true);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
